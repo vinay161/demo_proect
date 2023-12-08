ec4fc662b6bc694571a03af6de3cdb0be8212491
Vinay Kumar <vinay.k.lpu@gmail.com>
2023-12-08 10:35:13 +0530

    Add files via upload

---
+++ crud/crud/Data/ContactsAPIDbContext.cs
@@ -0,0 +1,14 @@
+﻿using crud.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace crud.Data
+{
+    public class ContactsAPIDbContext : DbContext
+    {
+        public ContactsAPIDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
